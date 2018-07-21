fd221b4747772bf6f10628c4ecc6ae7a7ebf7c5a
sergio <sergionirvana2010@hotmail.com>
2018-07-20 21:14:26 -0300

    Initial commit

---
+++ tests_functional.py
@@ -0,0 +1,30 @@
+import urllib3
+from app import app
+from selenium import webdriver
+from flask_testing import LiveServerTestCase
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+
+class TestSitePages(LiveServerTestCase):
+    def create_app(self):
+        app.config['LIVESERVER_PORT'] = 5000
+        return app
+
+    def setUp(self):
+        self._path = FirefoxBinary('/opt/firefox/firefox')
+        self.browser = webdriver.Firefox(firefox_binary=self._path)
+        self.base_url = self.get_server_url() + '/'
+        print(self.base_url)
+        self.http = urllib3.PoolManager()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_page_title(self):
+        self.browser.get(self.base_url)
+        home_page_title = self.browser.find_element_by_tag_name('h1').text
+        self.assertEqual(home_page_title, 'Todo List')
+
+    def test_home_page_status(self):
+        response = self.http.request(url=self.base_url, method='GET')
+        self.assertEqual(response.status, 200)
